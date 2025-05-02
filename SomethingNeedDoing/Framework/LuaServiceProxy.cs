2a928fec9ed7374a09b2bdacb9a6e5fd8598861b
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-05-02 16:20:35 +0100

    invoke safely

--- SomethingNeedDoing/Framework/LuaServiceProxy.cs
+++ SomethingNeedDoing/Framework/LuaServiceProxy.cs
@@ -14,38 +14,56 @@ public class LuaServiceProxy
 
         public override bool TryGetMember(GetMemberBinder binder, out object? result)
         {
-            if (typeof(Svc).GetProperty(serviceName)?.GetValue(null) is not { } service)
+            try
             {
+                if (typeof(Svc).GetProperty(serviceName)?.GetValue(null) is not { } service)
+                {
+                    result = null;
+                    return false;
+                }
+
+                if (service.GetType().GetProperty(binder.Name) is not { } property)
+                {
+                    result = null;
+                    return false;
+                }
+
+                result = property.GetValue(service);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Svc.Log.Error(ex, $"Failed to get {serviceName}.{binder.Name}");
                 result = null;
                 return false;
             }
-
-            if (service.GetType().GetProperty(binder.Name) is not { } property)
-            {
-                result = null;
-                return false;
-            }
-
-            result = property.GetValue(service);
-            return true;
         }
 
         public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result)
         {
-            if (typeof(Svc).GetProperty(serviceName)?.GetValue(null) is not { } service)
+            try
             {
+                if (typeof(Svc).GetProperty(serviceName)?.GetValue(null) is not { } service)
+                {
+                    result = null;
+                    return false;
+                }
+
+                if (service.GetType().GetMethod(binder.Name) is not { } method)
+                {
+                    result = null;
+                    return false;
+                }
+
+                result = method.Invoke(service, args);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Svc.Log.Error(ex, $"Failed to invoke {serviceName}.{binder.Name}");
                 result = null;
                 return false;
             }
-
-            if (service.GetType().GetMethod(binder.Name) is not { } method)
-            {
-                result = null;
-                return false;
-            }
-
-            result = method.Invoke(service, args);
-            return true;
         }
     }
 
