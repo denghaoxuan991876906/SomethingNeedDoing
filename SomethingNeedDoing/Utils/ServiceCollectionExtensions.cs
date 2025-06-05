2586a70658c12dab4feaf0a334dde1a058c1c813
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-06-05 13:07:40 +0100

    add scrutor

--- SomethingNeedDoing/Utils/ServiceCollectionExtensions.cs
+++ SomethingNeedDoing/Utils/ServiceCollectionExtensions.cs
@@ -1,14 +1,6 @@
-using Microsoft.Extensions.DependencyInjection;
-using SomethingNeedDoing.Gui;
-using SomethingNeedDoing.External;
 using Dalamud.Interface.Windowing;
+using Microsoft.Extensions.DependencyInjection;
 using SomethingNeedDoing.Core.Interfaces;
-using SomethingNeedDoing.NativeMacro;
-using SomethingNeedDoing.LuaMacro;
-using SomethingNeedDoing.Scheduler;
-using SomethingNeedDoing.Managers;
-using SomethingNeedDoing.Documentation;
-using SomethingNeedDoing.Gui.Tabs;
 
 namespace SomethingNeedDoing.Utils;
 
@@ -16,41 +8,29 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddSomethingNeedDoingServices(this IServiceCollection services)
     {
-        // Core Services
-        services.AddSingleton<MacroParser>();
-        services.AddSingleton<NativeMacroEngine>();
-        services.AddSingleton<NLuaMacroEngine>();
-        services.AddSingleton<LayLuaMacroEngine>();
-        services.AddSingleton<TriggerEventManager>();
-        services.AddSingleton<IMacroScheduler, MacroScheduler>();
-        services.AddSingleton<IMacroEngine, NativeMacroEngine>();
-        services.AddSingleton<IMacroEngine, NLuaMacroEngine>();
-        services.AddSingleton<IMacroEngine, LayLuaMacroEngine>();
-        services.AddSingleton<LuaModuleManager>();
-        services.AddSingleton<GitMacroManager>();
-        services.AddSingleton<MacroHierarchyManager>();
-        services.AddSingleton<LuaDocumentation>();
-
-        // UI Services
         services.AddSingleton<WindowSystem>();
-        services.AddSingleton<MacroStatusWindow>();
-        services.AddSingleton<HelpLuaTab>();
-        services.AddSingleton<HelpTab>();
-        services.AddSingleton(sp => new MacroEditor(
-            sp.GetRequiredService<IMacroScheduler>(),
-            sp.GetRequiredService<GitMacroManager>(),
-            sp.GetRequiredService<MacroStatusWindow>()
-        ));
-        services.AddSingleton(sp => new MainWindow(
-            sp.GetRequiredService<IMacroScheduler>(),
-            sp.GetRequiredService<MacroEditor>(),
-            sp.GetRequiredService<HelpTab>()
-        ));
 
-        // External Services
-        services.AddSingleton<Tippy>();
+        services.Scan(scan => scan
+            .FromAssemblyOf<Plugin>()
+            .AddClasses()
+            .AsSelf()
+            .WithSingletonLifetime()
+            .AddClasses(classes => classes
+                .AssignableTo<IMacroEngine>())
+            .AsImplementedInterfaces()
+            .WithSingletonLifetime()
+            .AddClasses(classes => classes
+                .AssignableTo<IMacroScheduler>())
+            .AsImplementedInterfaces()
+            .WithSingletonLifetime());
 
-        // Framework Services
+        services.Scan(scan => scan
+            .FromAssemblyOf<Plugin>()
+            .AddClasses(classes => classes
+                .AssignableTo<Window>())
+            .AsSelf()
+            .WithSingletonLifetime());
+
         services.AddTransient<MacroContext>();
 
         return services;
