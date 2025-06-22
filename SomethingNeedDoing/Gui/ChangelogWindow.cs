12a4d7e2a2cd9d24fcebf266ec131439251a3179
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-06-22 19:36:51 +0100

    Update ChangelogWindow.cs

--- SomethingNeedDoing/Gui/ChangelogWindow.cs
+++ SomethingNeedDoing/Gui/ChangelogWindow.cs
@@ -69,6 +69,7 @@ public class ChangelogWindow : Window
         Add("12.15", "Added syntax highlighting to the editor");
         Add("12.16", "Added type change ability in the editor");
         Add("12.17", "Fixed Craftloop, and some action skip/waits");
+        Add("12.18", "Fixed autoretainer post process event");
     }
 
     private void Add(string version, string description)
