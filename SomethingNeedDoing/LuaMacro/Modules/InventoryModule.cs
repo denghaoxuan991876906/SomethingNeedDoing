0e7318753cc03377866a0c5063bfe0a76f3f26bf
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-09-07 16:06:23 +0100

    Update InventoryModule.cs

--- SomethingNeedDoing/LuaMacro/Modules/InventoryModule.cs
+++ SomethingNeedDoing/LuaMacro/Modules/InventoryModule.cs
@@ -195,7 +195,7 @@ public unsafe class InventoryModule : LuaModuleBase
 
         [LuaDocs]
         [Changelog("12.51")]
-        [Changelog("13.46", ChangelogType.Fixed, "Stop fake item movement")]
+        [Changelog("13.46", ChangelogType.Fixed, "Potential fix for fake movement")]
         public void MoveItemSlot(InventoryType destinationContainer)
             => InventoryManager.Instance()->MoveItemSlot(Container, (ushort)Slot, destinationContainer, GetFirstEmptySlot(destinationContainer), true);
     }
